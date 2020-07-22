ff6a031e56541fc2307e8275ac43e1b0f2f59c21
HamidReazaNikoonia <hamidrezanik00ni@gmail.com>
2020-07-22 04:46:52 +0430

    Implement FieldWrapper component

--- src/container/views/home.vue
+++ src/container/views/home.vue
@@ -1,18 +1,22 @@
 <script>
 import appConfig from '@src/app.config'
 import Layout from '@layouts/main.vue'
+import FieldWrapper from '@components/Forms/FieldWrapper/index.vue'
 
 export default {
   page: {
     title: 'Home',
     meta: [{ name: 'description', content: appConfig.description }],
   },
-  components: { Layout },
+  components: { Layout, FieldWrapper },
 }
 </script>
 
 <template>
   <Layout>
+    <FieldWrapper>
+      salam
+    </FieldWrapper>
     <h1>Home Page</h1>
     <img src="@assets/images/logo.png" alt="Logo" />
   </Layout>
